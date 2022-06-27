7fa6081bfa51ab11825ec1c30d102148dd0b3827
ericdwkim <ericdwkim.io@gmail.com>
2022-06-27 11:50:17 -0500

    Test - 1

---
+++ code/test.py
@@ -0,0 +1,16 @@
+import nsfg
+
+df = nsfg.ReadFemPreg()
+# print(df)
+
+# print(df.columns[1])
+
+# pregordr = df['pregordr']
+# print(pregordr)
+# print(type(pregordr))
+
+# print(pregordr[0])
+# print(pregordr[2:5])
+
+pregordr = df.pregordr
+print(pregordr)
