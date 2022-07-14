fadd877571fc73289a75edcbc33b4ba98d8e895d
ericdwkim <ericdwkim.io@gmail.com>
2022-07-14 08:24:38 -0500

    Understanding the items() method after selecting a column from a df

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,13 +13,54 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+    dat_file='2002FemResp.dat.gz',
+    nrows=None
+):
+    """
+    Reads the NSFG *respondent* data
+    
+    @params `dct_file`: strata dict string file name
+    @params `dat_file`: string file name
+    @returns `DataFrame` 
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+def CleanFemResp(df):
+    pass
+
+def ValidatePregnum(resp):
+
+    """
+    Read preg dataframe
+    @returns preg df
+    """
+    preg = nsfg.ReadFemPreg()
+    # print(preg.head()) - first 5 rows of pregnancy dataframe
+
+    # Map caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+    # print(type(preg_map)) - default dict mapping of {caseid: [idx]} 
+
+    # iterate through the respondent `pregnum` series
+    for idx, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[idx]
+        
+
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+
+    resp = ReadFemResp()
+    ValidatePregnum(resp)
+
+    # print('%s: All tests passed.' % script)
 
 
 if __name__ == '__main__':
